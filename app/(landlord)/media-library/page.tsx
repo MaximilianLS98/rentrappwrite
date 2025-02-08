4c937089a3fdb01d99fd6b84bd5805a5290fbf78
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-08 23:39:33 +0100

    Added a media library page

---
+++ app/(landlord)/media-library/page.tsx
@@ -0,0 +1,26 @@
+import { axiosInstanceClient } from "@/utils/clientAxios"
+import { cookies } from "next/headers"
+import AppwriteImage from "@/components/appwriteImage"
+
+
+export default async function() { 
+    const allCookies = await cookies();
+
+    const { data } = await axiosInstanceClient.get('/api/bucket/images', {
+        headers: {
+			cookie: `session=${allCookies.get('session')?.value}`,
+		},
+    });    
+
+    return (
+        <main className="container m-auto">
+            <h1 className="text-7xl my-8">Mediebibliotek</h1>
+            <div className='flex gap-4 flex-wrap'>
+                {data.files.map((file: any) => {
+                    return <AppwriteImage fileId={file.$id} alt={file.name} key={file.$id} />;
+                })}
+                {data.files.length === 0 && <p>Ingen bilder funnet</p>}
+            </div>
+        </main>
+    )
+}
