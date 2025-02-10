303be90c605f82bd7a7943df2e4e2a34b5c4552b
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-10 19:23:45 +0100

    Added properties model and views

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { axiosInstance } from '@/utils/axios';
+import { cookies } from 'next/headers';
+import { Card, CardContent, CardFooter, CardHeader, CardDescription } from '@/components/ui/card';
+import { getPropertyById } from '@/actions/properties';
+
+type Property = {
+	name: string;
+	address: string;
+	type: string;
+	owner: string;
+	$id: string;
+	$createdAt: string;
+	$updatedAt: string;
+	$permissions: string[];
+	$databaseId: string;
+	$collectionId: string;
+};
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function Page(props:Props) {
+    const { id } = await props.params;
+	const session = (await cookies()).get('session')?.value as string;
+    
+    const property = await getPropertyById(session, id) as Property;
+
+	return (
+		<div className='container mt-8'>
+			<h1 className='text-5xl'>{property.name}</h1>
+            <Card className='shadow-md'>
+                <CardHeader className='text-4xl'>{property.name}</CardHeader>
+                <CardContent>
+                    <CardDescription>{property.address}</CardDescription>
+                </CardContent>
+                <CardFooter>{property.type}</CardFooter>
+            </Card>
+			<pre>{JSON.stringify(property, null, 2)}</pre>
+		</div>
+	);
+}
