5f8d4a1b8893a16ded6b5ed47e2e3166b654a85e
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-13 11:36:47 +0100

    Added a link to properties list view

--- components/PropertyOverview.tsx
+++ components/PropertyOverview.tsx
@@ -1,6 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Building, Home, DollarSign, Users } from 'lucide-react';
 import KeyMetrics from './landlordDashboard/KeyMetrics';
+import { Button } from './ui/button';
+import Link from 'next/link';
 
 interface PropertyOverviewProps {
 	totalProperties: number;
@@ -19,27 +21,35 @@ export function PropertyOverview({
 	properties,
 	units,
 }: PropertyOverviewProps) {
-
 	const totalRent = units.documents.reduce((acc: number, unit: any) => {
 		return acc + unit.monthlyrent;
 	}, 0);
 
-  const totalTenantsCounted = units.documents.filter((unit: any) => unit.status === 'Occupied').length;
+	const totalTenantsCounted = units.documents.filter(
+		(unit: any) => unit.status === 'Occupied',
+	).length;
 
-  const formatCurrency = (value: number) => {
-    return new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(value);
-  }
+	const formatCurrency = (value: number) => {
+		return new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(value);
+	};
 
 	return (
 		<div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
-			<Card>
-				<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-					<CardTitle className='text-sm font-medium'>Eiendommer</CardTitle>
-					<Building className='h-4 w-4 text-muted-foreground' />
+			<Card className='relative'>
+				<CardHeader className='pb-2'>
+					<div className='flex flex-row items-center justify-between spacy-y-0'>
+						<CardTitle className='text-sm font-medium'>Eiendommer</CardTitle>
+						<Building className='h-4 w-4 text-muted-foreground' />
+					</div>
 				</CardHeader>
 				<CardContent>
 					<div className='text-2xl font-bold mb-2'>{properties.total}</div>
 				</CardContent>
+				<CardFooter className='absolute bottom-1 left-1 p-4'>
+					<Link href='/properties/list'>
+						<Button variant='outline'>Alle eiendommer</Button>
+					</Link>
+				</CardFooter>
 			</Card>
 			{/* <Card>
 				<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
