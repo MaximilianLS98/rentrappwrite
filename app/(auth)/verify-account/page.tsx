62af250b52825d5c4b0e6a660a5bd90affa330a9
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-15 09:14:43 +0100

    A page for sending verification email, should not be used

--- app/(auth)/verify-account/page.tsx
+++ app/(auth)/verify-account/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { createSessionClient } from '@/appwrite/config';
+import { sendVerificationEmail } from '@/actions/verification';
 import { cookies } from 'next/headers';
 import auth from '@/utils/auth';
 
@@ -7,21 +8,30 @@ export default async function Page() {
 	const session = (await cookies()).get('session')?.value as string;
     const user = await auth.getUser();
 
-	const sendVerificationEmail = async () => {
+    const result = async () => {
         if (user.emailVerification || user.phoneVerification) {
             console.log('User is already verified');
-            return;
+            return { message: 'User is already verified', status: 200 };
         }
-        const { account } = await createSessionClient(session);
-        const result = await account.createVerification('http://localhost:3000/verify');
-        console.log(result);
-	};
-    sendVerificationEmail();
+        return await sendVerificationEmail();
+    }
+    const resolvedResult = await result();
+
+	// const sendVerificationEmail = async () => {
+    //     if (user.emailVerification || user.phoneVerification) {
+    //         console.log('User is already verified');
+    //         return;
+    //     }
+    //     const { account } = await createSessionClient(session);
+    //     const result = await account.createVerification('http://localhost:3000/verify');
+    //     console.log(result);
+	// };
+    // sendVerificationEmail();
 	return (
 		<main className='container mx-auto p-4'>
 			<h1 className='text-5xl'>Verify account</h1>
             <pre>{JSON.stringify(user, null, 2)}</pre>
-			<Button>Meningsløs knapp</Button>
+            <pre>{JSON.stringify(resolvedResult, null, 2)}</pre>
 		</main>
 	);
 }
