153c78466c71ad4c030010385b171d1f9e7b2ce6
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-22 14:12:24 +0100

    Changed the styling for container to the parent page instead of component

--- app/properties/dashboard.tsx
+++ app/properties/dashboard.tsx
@@ -64,7 +64,7 @@ export default function Dashboard(props: Props) {
   }
   
 	return (
-		<div className='container mx-auto p-4 space-y-4'>
+		<div className='mx-auto  space-y-4'>
 			<div className='flex justify-between items-center'>
 				<h1 className='text-3xl font-bold'>Dashboard</h1>
 				<div className='space-x-2'>
