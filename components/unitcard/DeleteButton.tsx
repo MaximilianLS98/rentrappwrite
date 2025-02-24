9338c650d55a9a74d6d003fbcbe93f48dedcafa9
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-02-24 18:38:39 +0100

    added alert dialog (for deleting units)

--- components/unitcard/DeleteButton.tsx
+++ components/unitcard/DeleteButton.tsx
@@ -4,52 +4,88 @@ import { axiosInstanceClient } from '@/utils/clientAxios';
 import { useRouter } from 'next/navigation';
 import { useToast } from '@/hooks/use-toast';
 import { Trash2 } from 'lucide-react';
-
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
 // ! - ONLY WORKS FOR DELETING UNITS AS OF NOW
 // TODO - Refactor so it can delete from any collection
-export default function DeleteButton(props: { id: string, redirect?: boolean, setUnits?: any }) {
-    const router = useRouter();
-    const { toast } = useToast();
+export default function DeleteButton(props: { id: string; redirect?: boolean; setUnits?: any }) {
+	const router = useRouter();
+	const { toast } = useToast();
 
 	const handleDelete = async (e: any) => {
-        e.preventDefault();
+		e.preventDefault();
 		console.log(`Deleting unit with id: ${props.id}`);
 
-        try {
-            const response = await axiosInstanceClient.delete(`api/units/${props.id}`);
-            console.log(`Response from delete request: ${JSON.stringify(response)}`);
-            if (props.redirect) {
-                router.push('/units');
-                router.refresh();
-            } else {
-                router.refresh();
-            }
-            if (props.setUnits) {
-                props.setUnits( (prev: any) => { return prev.filter((unit: any) => unit.$id !== props.id) });
-            }
-            toast({
-                title: 'Unit Deleted',
-                description: 'Unit was deleted successfully',
-            });
-        } catch (error) {
-            console.error(error);
-            toast({
-                title: 'Error',
-                description: 'Could not delete unit',
-                variant: 'destructive',
-            });
-        }
+		try {
+			const response = await axiosInstanceClient.delete(`api/units/${props.id}`);
+			console.log(`Response from delete request: ${JSON.stringify(response)}`);
+			if (props.redirect) {
+				router.push('/units');
+				router.refresh();
+			} else {
+				router.refresh();
+			}
+			if (props.setUnits) {
+				props.setUnits((prev: any) => {
+					return prev.filter((unit: any) => unit.$id !== props.id);
+				});
+			}
+			toast({
+				title: 'Unit Deleted',
+				description: 'Unit was deleted successfully',
+			});
+		} catch (error) {
+			console.error(error);
+			toast({
+				title: 'Error',
+				description: 'Could not delete unit',
+				variant: 'destructive',
+			});
+		}
 	};
 
 	return (
 		<>
-			{/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
-			<Button className='w-full rounded' onClick={(e) => handleDelete(e)} variant={'destructive'}>
-                {/* <Trash2 size={16} className='mr-1' /> */}
-                <Trash2 size={16} className='' />
-				{/* Slett */}
-			</Button>
+			{/* <Button
+				className='w-full rounded'
+				onClick={(e) => handleDelete(e)}
+				variant={'destructive'}>
+				<Trash2 size={16} className='' />
+			</Button> */}
+			<AlertDialog>
+				<AlertDialogTrigger asChild className='bg-destructive rounded'>
+					<Button className='w-full rounded' variant={'destructive'}>
+						<Trash2 size={16} className='' />
+						Slett
+					</Button>
+				</AlertDialogTrigger>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>
+							Er du sikker på at du vil slette denne enheten?
+						</AlertDialogTitle>
+						<AlertDialogDescription>
+							Dette kan ikke omgjøres. All informasjon om enheten vil bli slettet.
+						</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogCancel>Avbryt</AlertDialogCancel>
+						<AlertDialogAction className='bg-destructive' onClick={(e) => handleDelete(e)}>
+							Slett enhet
+						</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
 		</>
 	);
 }
