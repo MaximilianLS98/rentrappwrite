2df94006e7c143be912dd163d42c51a11805766f
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-02 22:47:21 +0100

    Better form for creating a new lease, functional and links up with a unit

--- app/(landlord)/leases/create/page.tsx
+++ app/(landlord)/leases/create/page.tsx
@@ -1,29 +1,40 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RentalContractPDF from './generatePDF';
+import { createLease } from '@/actions/leases';
+import { getAllUnits } from '@/actions/units';
+import { getAllProperties } from '@/actions/properties';
+import { FetchUnit, TUnit } from '@/constants/types/units';
+import { TPropertyFetch, TProperty } from '@/constants/types/properties';
+import { parse } from 'path';
 
 export default function Page() {
+	const [allProperties, setAllProperties] = useState<TProperty[]>([]);
 	const [formData, setFormData] = useState({
-		landlordName: '',
-		tenantName: '',
+		landlord_id: '',
+		tenant_id: '',
 		propertyAddress: '',
-		startDate: '',
-		endDate: '',
-		rentAmount: '',
-		securityDeposit: '',
-		terms: '',
+		start_date: '',
+		end_date: '',
+		monthly_rent: '',
+		additional_terms: '',
+		active: true,
+		properties: '',
+		units: '',
 	});
-    const [submitted, setSubmitted] = useState(false);
+	const [submitted, setSubmitted] = useState(false);
 
 	interface FormData {
-		landlordName: string;
-		tenantName: string;
+		landlord_id: string;
+		tenant_id: string;
 		propertyAddress: string;
-		startDate: string;
-		endDate: string;
-		rentAmount: string;
-		securityDeposit: string;
-		terms: string;
+		start_date: string;
+		end_date: string;
+		monthly_rent: string;
+		additional_terms: string;
+		active: boolean;
+		properties?: string;
+		units: string;
 	}
 
 	interface ChangeEvent {
@@ -33,143 +44,143 @@ export default function Page() {
 		};
 	}
 
+	useEffect(() => {
+		const fetchProperties = async () => {
+			const response = await fetch('/api/cookie');
+			const sessionCookie = await response.json();
+			const properties = (await getAllProperties(sessionCookie.session)) as TPropertyFetch;
+			setAllProperties(properties.documents);
+		};
+		fetchProperties();
+	}, []);
+
 	const handleChange = (e: ChangeEvent) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const parseFormData = (formData: FormData) => {
+        const timeNow = new Date().toISOString();
+        const parsedFormData = {
+            landlord_id: formData.landlord_id,
+            tenant_id: formData.tenant_id,
+            // propertyAddress: formData.propertyAddress,
+            start_date: formData.start_date,
+            end_date: formData.end_date,
+            monthly_rent: parseInt(formData.monthly_rent, 10),
+            additional_terms: formData.additional_terms,
+            active: formData.active,
+            // properties: formData.properties,
+            units: formData.units,
+            unit_id: formData.units,
+            status: timeNow > formData.end_date ? 'expired' : 'draft',
+        }
+        return parsedFormData;
+    }
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-        setSubmitted(true);
+        const leaseObj = parseFormData(formData);
+        const sessionCookie = await fetch('/api/cookie').then(res => res.json());
+        createLease(sessionCookie.session, leaseObj);
+		setSubmitted(true);
 		console.log('Form submitted', formData);
 	};
 
 	return (
 		<div className='container mx-auto py-8'>
-			<h1 className='text-5xl text-rentr-darkblue mb-4'>Rental Agreement Form</h1>
+			<h1 className='text-5xl text-rentr-darkblue mb-4'>Sett opp ny leiekontrakt</h1>
 			<div className='flex'>
 				<div className='w-1/2'>
-					<form
-						onSubmit={handleSubmit}
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							gap: '10px',
-							maxWidth: '400px',
-							margin: '0 auto',
-						}}>
+					<form onSubmit={handleSubmit} className='flex flex-col gap-2 max-w-xs mx-auto'>
 						<input
 							type='text'
-							name='landlordName'
+							name='landlord_id'
 							placeholder='Landlord Name'
-                            disabled={submitted}
+							disabled={submitted}
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
 						<input
 							type='text'
-							name='tenantName'
+							name='tenant_id'
 							placeholder='Tenant Name'
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
+						<select
+							name='properties'
+							onChange={handleChange}
+							required
+							className='p-2 rounded border border-gray-300'>
+							<option value=''>Choose a property</option>
+							{allProperties.map((property) => (
+								<option key={property.$id} value={property.$id}>
+									{property.name}
+								</option>
+							))}
+						</select>
+						<select
+							name='units'
+							onChange={handleChange}
+							required
+							className='p-2 rounded border border-gray-300'>
+							<option value=''>Velg en enhet</option>
+							{/* I need to list out every unit, but only the ones for the currently selected property, saved as "properties" in formdata state */}
+							{
+								formData.properties && allProperties.find(property => property.$id === formData.properties)?.units?.map((unit) => (
+                                    <option key={unit.$id} value={unit.$id}>
+                                        {unit.title}
+                                    </option>
+                                ))
+                            }
+						</select>
 						<input
 							type='text'
 							name='propertyAddress'
 							placeholder='Property Address'
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
 						<input
 							type='date'
-							name='startDate'
+							name='start_date'
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
 						<input
 							type='date'
-							name='endDate'
+							name='end_date'
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
 						<input
 							type='number'
-							name='rentAmount'
+							name='monthly_rent'
 							placeholder='Monthly Rent'
 							onChange={handleChange}
 							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
-						/>
-						<input
-							type='number'
-							name='securityDeposit'
-							placeholder='Security Deposit'
-							onChange={handleChange}
-							required
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-							}}
+							className='p-2 rounded border border-gray-300'
 						/>
 						<textarea
-							name='terms'
+							name='additional_terms'
 							placeholder='Additional Terms'
 							onChange={handleChange}
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: '1px solid #ccc',
-								minHeight: '100px',
-							}}></textarea>
+							className='p-2 rounded border border-gray-300 min-h-[100px]'></textarea>
 						<button
 							type='submit'
-                            disabled={submitted}
-							style={{
-								padding: '10px',
-								borderRadius: '5px',
-								border: 'none',
-								backgroundColor: '#007BFF',
-								color: '#fff',
-								cursor: 'pointer',
-							}}>
-							Generate Contract
+							disabled={submitted}
+							className='p-2 rounded border-none bg-blue-500 text-white cursor-pointer disabled:bg-slate-500 disabled:cursor-not-allowed'>
+							Opprett kontrakt
 						</button>
 					</form>
 				</div>
-				<div className='w-1/2'>
-					{submitted && (
-						<RentalContractPDF formData={formData} />
-					)}
-				</div>
+				<div className='w-1/2'></div>
+				{submitted && <RentalContractPDF formData={formData} />}
 			</div>
 		</div>
 	);
