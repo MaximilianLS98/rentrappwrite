96d1c78f8d4d42c634a8284b060aacc0c210d3f0
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-02 22:49:43 +0100

    Delete property button now with confirmation dialog

--- app/properties/[id]/DeletePropertyButton.tsx
+++ app/properties/[id]/DeletePropertyButton.tsx
@@ -2,6 +2,19 @@
 
 import { deletePropertyById } from '@/actions/properties';
 import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Trash2 } from 'lucide-react';
 
 type Props = {
 	propertyId: string;
@@ -9,19 +22,44 @@ type Props = {
 
 export default function DeletePropertyButton(props: Props) {
 	const deleteProperty = async (id: string) => {
-        try {
-            const sessionCookie = (await fetch('/api/cookie').then((res) => res.json())) || '';
-            // console.log(`Session cookie in DeletePropertyButton: ${JSON.stringify(sessionCookie)}`);
-            const result = await deletePropertyById(sessionCookie.session, id);
-            // console.log(`Result of deletePropertyById: ${JSON.stringify(result)}`);
-        } catch {
-            console.error('Error deleting property');
-        }
+		const sessionCookie = (await fetch('/api/cookie').then((res) => res.json())) || '';
+		const result = await deletePropertyById(sessionCookie.session, id);
+		if (result.error) {
+			toast.error('Kunne ikke slette eiendommen');
+		} else {
+			toast.success('Eiendommen ble slettet');
+		}
 	};
 
 	return (
-		<Button variant='destructive' onClick={() => deleteProperty(props.propertyId)}>
-			Delete
-		</Button>
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button variant='destructive'>
+                    <Trash2 size={16} />
+					Slett eiendom
+				</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Er du sikker?</AlertDialogTitle>
+				</AlertDialogHeader>
+				<AlertDialogDescription>
+					Dette vil slette eiendommen permanent. Denne handlingen kan ikke angres.
+				</AlertDialogDescription>
+				<AlertDialogFooter>
+					<AlertDialogAction asChild>
+						<Button
+							variant='destructive'
+							onClick={() => deleteProperty(props.propertyId)}
+                            className='bg-destructive'>
+							Slett eiendom
+						</Button>
+					</AlertDialogAction>
+					<AlertDialogCancel asChild>
+						<Button variant='secondary'>Avbryt</Button>
+					</AlertDialogCancel>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	);
 }
