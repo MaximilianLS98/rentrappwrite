501c6412de876480479d2633432eb0e14a31ceb1
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-07 10:40:04 +0100

    Removed unused code and changed the hover styles

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -1,19 +1,12 @@
 import Link from 'next/link';
-
 import { menuConstants } from '../../constants/menuConstants';
-import type { MenuElement } from '@/constants/menuConstants';
-// import { ModeToggle } from '@/components/ThemeSwitcher';
 import { ModeToggle } from '../ThemeSwitcher';
 import { Button } from '../ui/button';
 import auth from '@/utils/auth';
 import LogoutButton from '@/components/header/LogoutButton';
 
-// import { UserButton } from '@clerk/nextjs';
-// import { getUserAuth } from '@/lib/auth/utils';
-
 export default async function Menu() {
 	const user = await auth.getUser();
-	// console.log('User:', user);
 	
 
 	const UserSection = user ? (
@@ -35,17 +28,13 @@ export default async function Menu() {
 							<Link
 								key={menu.id}
 								href={menu.href}
-								className={`my-auto text-xl hover:underline`}>
+								className={`my-auto text-xl hover:text-rentr-main`}>
 								{menu.name}
 							</Link>
 						);
 					})}
 					<div className='my-auto'>
 						{UserSection}
-						{/* <Link href='/login'>
-							<Button variant='outline'>Login</Button>
-						</Link> */}
-						{/* <UserButton afterSignOutUrl='/' /> */}
 					</div>
 					<ModeToggle />
 				</div>
