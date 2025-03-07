d324c495a11a1ff73cc9e3934cf8acb6ef4e73d8
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-07 10:40:21 +0100

    Added new hover styling to rentr variant of button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        rentr: "bg-rentr-main text-white hover:bg-rentr-darkblue",
+        rentr: "bg-rentr-main text-white hover:bg-rentr-darkblue hover:border-rentr-main",
       },
       size: {
         default: "h-10 px-4 py-2",
